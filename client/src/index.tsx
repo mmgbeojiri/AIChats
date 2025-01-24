885071b740a59cf6b0087479f58d34972da368d6
mmgbeojiri <michaelm1464@nycstudents.net>
2025-01-24 14:43:56 -0500

    memory json now correlates to the frontend

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,30 @@
-import { useState } from 'react'
+import {useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
+import jsonData from "../../memory.json"
 
 const name: string = 'Glem'
 
+interface Message {
+  role: string;
+  content: string;
+}
+
 function Index() {
   const [count, setCount] = useState(0)
+  const [messageData, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    // Read and parse the memory.json file
+    setMessages(jsonData[0].messages);
+  }, []);
 
   return (
     <>
     <div className='max-w-4xl flex flex-col justify-center mx-auto space-between h-screen'>
       <div>
-        <h1 className='p-4 rounded-xl mx-4 my-2 font-semibold bg-white shadow-sm'>{name}</h1>
+        <h1 className='p-4 rounded-xl mx-4 mt-2 font-semibold bg-white shadow-sm'>{name}</h1>
       </div>
 
       <div className='messages'>
@@ -20,6 +32,16 @@ function Index() {
             <h1 className='message-role'>System</h1>
             <h1 className='message-content'>Hello world! <span>Shits on you cutely.</span></h1>
             </div>
+          {
+            messageData.map((messageData, index) => {
+              return (
+                <div key={index} className='message'>
+                  <h1 className='message-role'>{messageData.role}</h1>
+                  <h1 className='message-content'>{messageData.content}</h1>
+                </div>
+              )
+            })
+          }
       </div>
 
       <div className='flex justify-center gap-1 m-4 bg-white p-2 rounded-xl shadow-sm'>
