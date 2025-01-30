fb8145225ed17185c0d01f5628a019f040052106
mmgbeojiri <michaelm1464@nycstudents.net>
2025-01-30 14:45:45 -0500

    lets see here

--- responseAPI.js
+++ responseAPI.js
@@ -1,68 +1,46 @@
 // run `node index.js` in the terminal
-import * as chatAPI from './chatAPI.js';
+import {createCompletion, loadModel} from 'C:/Users/MichaelMgbeojirikwe/OneDrive - NYCDOE/Desktop/FullStackDevelopment/AIGF/client/node_modules/gpt4all/src/gpt4all.js'
 
 let data = JSON.parse(fs.readFileSync('./memory.json', 'utf8'));
 
 let debugMode = true;
 
-/*const addToDatabase = (role, newMessage) => {
-  let data = JSON.parse(fs.readFileSync('./memory.json', 'utf8')); // Read the existing data
+const reponseModel = await loadModel('orca-mini-3b-gguf2-q4_0.gguf', {
+  verbose: debugMode,
+  device: "gpu",
+  nCtx: 2048,
+})
+const personalityPrompt = "### System:\nYou are a fantasy game master. The setting is a magical fantasy world called Eldoria. You are the assistant Glem, a man who will assist the player through this world.\n\n";
+const reponseChatData = await reponseModel.createChatSession({
+  temperature: 1,
+  systemPrompt: personalityPrompt
+})
 
-  data[0].messages = [...data[0].messages, { role: role, content: newMessage }];
+// Load External Memory
 
-  fs.writeFile('memory.json', JSON.stringify(data), function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
-};*/
 
-const ask = () => {
+const ask = async () => {
   let newMessage = prompt('User: ');
-  chatAPI.addToDatabase('user', newMessage, 'memory.json');
-  let dataOutput = chatAPI.sendMessage(data, debugMode);
 
-  console.log(data.choices[0].message.content);
+  if (debugMode) {
+    console.log('sending request now');
+    console.log(dataInput);
+    console.log('waiting for response...');
+  }
+  
+  let reponseDataOutput = await createCompletion(reponseChatData, newMessage)
+
+  console.log('response received');
+  //chatAPI.addToDatabase('user', newMessage, 'memory.json');
+  //let dataOutput = chatAPI.sendMessage(data, debugMode);
+
+  console.log(reponseChatData.choices[0].message.content);
   chatAPI.addToDatabase(
-        data.choices[0].message.role,
-        data.choices[0].message.content,
+    reponseChatData.choices[0].message.role,
+    reponseChatData.choices[0].message.content,
         'memory.json'
       );
 };
 
-/*
-
-const send = async () => {
-  if (debugMode) {
-    console.log('sending request now');
-    console.log(data);
-    console.log('waiting for response...');
-  }
-
-  let response = await fetch(url, {
-    method: 'POST',
-    headers: headers,
-    body: JSON.stringify(data),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      if (debugMode) {
-      console.log(data)
-      }
-      console.log(data.choices[0].message.content);
-      addToDatabase(
-        data.choices[0].message.role,
-        data.choices[0].message.content
-      );
-    }).then(() => {
-      if (debugMode) {
-        console.log('response received');
-      }
-    })
-    .finally(() => {
-      ask();
-    });
-};
-
-*/
 ask();
+reponseModel.dispose();
