388000057d86e6176f3f607c1f17cb632ceb1246
mmgbeojiri <michaelm1464@nycstudents.net>
2025-02-11 14:51:47 -0500

    ok so maybe removing the fs library was bad

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,19 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
 export default defineConfig({
-plugins: [react()],
+  resolve: {
+    alias: {
+      fs: require.resolve('rollup-pligin-node-builtins'),
+      // http: require.resolve('rollup-plugin-node-builtins'),
+      // util: require.resolve('rollup-plugin-node-builtins'),
+      // stream: require.resolve('rollup-plugin-node-builtins'),
+      // buffer: require.resolve('rollup-plugin-node-builtins'),
+      // process: require.resolve('rollup-plugin-node-builtins'),
+      // url: require.resolve('rollup-plugin-node-builtins'),
+      // querystring: require.resolve('rollup-plugin-node-builtins'),
+    },
+  },
+  plugins: [react()],
   css: {
    postcss: {
     plugins: [tailwindcss()],
